7bfb5255f1abc2c168c184329e1efbff618cc734
the-it-weirdo <ddsmegh4@gmail.com>
2019-10-27 00:58:57 +0530

    implements NoteViewModel and NoteAdapter.

--- app/src/main/java/com/kingominho/notesapp_learningmvvm/MainActivity.java
+++ app/src/main/java/com/kingominho/notesapp_learningmvvm/MainActivity.java
@@ -2,13 +2,42 @@ package com.kingominho.notesapp_learningmvvm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView mRecyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        mRecyclerView.setHasFixedSize(true);
+
+        final NoteAdapter mNoteAdapter = new NoteAdapter();
+        mRecyclerView.setAdapter(mNoteAdapter);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                //update RecyclerView
+                //Toast.makeText(getApplicationContext(), "onChanged", Toast.LENGTH_SHORT).show();
+                mNoteAdapter.setNotes(notes);
+            }
+        });
+
+
     }
 }
